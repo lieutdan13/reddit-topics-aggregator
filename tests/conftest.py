1f631aa405ac40bf1d48a1477d9f1c7420e3da3a
Dan Schaefer <danschaefer@gmail.com>
2024-10-08 18:51:59 -0400

    clears env vars for clean tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import functools
+import os
 from collections.abc import Generator
 from types import FunctionType
 
@@ -12,3 +13,12 @@ from reddit_topics_aggregator.cli import reddit_topics_aggregator
 def cli() -> Generator[FunctionType]:
     runner = CliRunner()
     yield functools.partial(runner.invoke, reddit_topics_aggregator)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def clear_env_vars() -> None:
+    del os.environ["REDDIT_CLIENT_ID"]
+    del os.environ["REDDIT_CLIENT_SECRET"]
+    del os.environ["REDDIT_USERNAME"]
+    del os.environ["REDDIT_PASSWORD"]
+    del os.environ["REDDIT_USER_AGENT"]
